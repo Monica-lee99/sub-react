1e45780b85ae51d2ab471862e5eba3dc61754c9d
gohooli <gohooli@tencent.com>
2021-11-28 17:58:29 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+// micro-app-react/src/App.jsx
+const BASE_NAME = window.__POWERED_BY_QIANKUN__ ? "/sub-react" : "";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" basename={BASE_NAME}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
